6831a5a4938eaec8df84b5b59e00739a2f19a4ed
Witeroo Inc <witerooinc@Witeroo-Inc-MacBook-Pro.local>
2022-06-06 02:48:12 -0500

    first commit on main

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+    region = var.region
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+
+module "ecrm" {
+  source  = "app.terraform.io/ogacloud/ecrm/aws"
+  version = "0.0.1"
+  name = var.name
+  # insert required variables here
+}
+
+
+# Build Docker image and push to ECR from folder: 
+module "ecr_docker_build" {
+  source = "github.com/onnimonni/terraform-ecr-docker-build-module"
+
+  # Absolute path into the service which needs to be build
+  dockerfile_folder = "${path.module}"
+
+  # Tag for the builded Docker image (Defaults to 'latest')
+  docker_image_tag = "latest"
+
+  # ECR repository where we can push
+  ecr_repository_url = "${aws_ecr_repository.this.repository_url}"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+    type = string
+    description = "region located"
+}
+
+variable "name" {
+    type = string
+    description = "name"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  description = "Secret key for the account"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "Access secret key for the account"
+}
